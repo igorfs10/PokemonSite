d884199aa1e6d3437d41a741f25ad7ccfe38c8f0
igorfs10 <igorfs10@gmail.com>
2019-04-29 22:40:30 -0300

    Add files via upload
    Uso de constante para facilitar

--- pokemon.py
+++ pokemon.py
@@ -1,6 +1,9 @@
 import urllib.request
 
-for x in range (1, 810):
+PRIMEIRO_POKEMON = 1
+ULTIMO_POKEMON = 809
+
+for x in range (PRIMEIRO_POKEMON, ULTIMO_POKEMON + 1):
 	if x < 10:
 		urllib.request.urlretrieve("https://assets.pokemon.com/assets/cms2/img/pokedex/full/00" + str(x) + ".png", "00" + str(x) + ".jpg")
 	elif x < 100:
@@ -9,4 +12,4 @@ for x in range (1, 810):
 		urllib.request.urlretrieve("https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + str(x) + ".png", str(x) + ".jpg")
 	print(str(x) + " downloaded." )
 else:
-print("End.")
+	print("End.")
