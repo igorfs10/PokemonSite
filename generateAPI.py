8c1b46fad378f73f301e3f575ce4a535dcbe3884
igorfs10 <igorfs10@gmail.com>
2019-05-04 20:31:51 -0300

    Cria pasta automaticamente para o travis

--- generateAPI.py
+++ generateAPI.py
@@ -2,15 +2,24 @@ from distutils.dir_util import copy_tree
 import pandas as pd
 import numpy as np
 import json
+import os
 
 PRIMEIRO_POKEMON = 1
 ULTIMO_POKEMON = 807
 
+#Cria pasta API se não existir
+dirName = "api"
+try:
+    os.mkdir(dirName)
+    print("Directory " , dirName ,  " Created ") 
+except FileExistsError:
+    print("Directory " , dirName ,  " already exists")
+    
 #Abre o csv
 dataset = pd.read_csv("pokemons.csv")
 
 #Definindo a primeira coluna como o identificador de cada pokémon
-dataset.set_index("Id", inplace = True)
+#dataset.set_index("Id", inplace = True)
 
 for i in range (PRIMEIRO_POKEMON, ULTIMO_POKEMON + 1):
     data = dataset.iloc[i-1:i].to_json(orient = "records").replace("[", "").replace("]", "")
@@ -20,11 +29,15 @@ for i in range (PRIMEIRO_POKEMON, ULTIMO_POKEMON + 1):
     print(id + " generated.")
 
 #salva os dados numa string
-data = dataset.iloc[PRIMEIRO_POKEMON-1:ULTIMO_POKEMON+1].to_json(orient = "records").replace("[", "").replace("]", "")
+print(dataset[["Name"]])
+data = dataset[["Id", "Name"]].to_json(orient = "records").replace("[", "").replace("]", "")
 data = "{[" + data + "]}"
 
 #salva a string no arquivo
 with open("api/pokemons.json", "w") as file:
+    file.write(data)
+
+with open("api/index.json", "w") as file:
     file.write(data)
 
 copy_tree("api", "/PokemonSite/api")
