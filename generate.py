3f6c944babd9b749420a15d6458cb02a368a4065
igorfs10 <igorfs10@gmail.com>
2019-05-04 19:47:08 -0300

    Teste de gerar api

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+from distutils.dir_util import copy_tree
 import pandas as pd
 import numpy as np
 import json
@@ -11,9 +12,21 @@ dataset = pd.read_csv("pokemons.csv")
 #Definindo a primeira coluna como o identificador de cada pokémon
 dataset.set_index("Id", inplace = True)
 
+for i in range (PRIMEIRO_POKEMON, ULTIMO_POKEMON + 1):
+    data = dataset.iloc[i-1:i].to_json(orient = "records").replace("[", "").replace("]", "")
+    id = str(i)
+    with open("api/" + id +".json", "w") as file:
+        file.write(data)
+    print(id + " generated.")
+
 #salva os dados numa string
 data = dataset.iloc[PRIMEIRO_POKEMON-1:ULTIMO_POKEMON+1].to_json(orient = "records").replace("[", "").replace("]", "")
+data = "{[" + data + "]}"
 
 #salva a string no arquivo
 with open("api/pokemons.json", "w") as file:
     file.write(data)
+
+copy_tree("api", "/PokemonSite/api")
+
+print("API generated.")
