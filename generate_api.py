60f9b15c8995a82a8e9245aeef7eafec91bfb543
Igor Feitosa Sampaio <igorfs10@gmail.com>
2019-07-05 14:20:46 -0300

    Update generate_api.py

--- generate_api.py
+++ generate_api.py
@@ -25,7 +25,7 @@ ULTIMO_POKEMON = dataset.shape[0]
 
 for i in range (PRIMEIRO_POKEMON, ULTIMO_POKEMON + 1):
     data = dataset.iloc[i-1:i].to_json(orient = "records").replace("[", "").replace("]", "").replace("\/", "/")
-    id = str(dataset.iloc[i-1:i,0])
+    id = str(dataset.iloc[i-1:i,0].values[0])
     with open("api/" + id +".json", "w") as file:
         file.write(data)
     print(id + " generated.")
