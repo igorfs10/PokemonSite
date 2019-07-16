88defc11db327230e96c602b7fafab61e4ea57c2
igorfs10 <igorfs10@gmail.com>
2019-07-16 00:02:15 -0300

    Pagina usando a API e Vuejs

---
+++ js/app.js
@@ -0,0 +1,78 @@
+"use strict";
+
+var url = 'https://igorfs10.github.io/PokemonSite/api/';
+
+var pokemons;
+fetch(url).then(res => res.json()).then((out) => {pokemons = out.pokemons;}).catch(err => { throw err });
+
+new Vue({
+    el: "#app",
+    data: {
+        pokemon: {
+            name: null,
+            id: 0,
+            type_primary: null,
+            type_secondary: null,
+            image: "https://raw.githubusercontent.com/igorfs10/PokemonSite/gh-pages/images/0.png"
+        },
+        cores: {"background-image": "linear-gradient(#68A090, #68A090)"}
+    },
+    methods: {
+        changePokemon(event){
+            if(event.target.value < pokemons.length){
+                this.pokemon = pokemons[event.target.value];
+            }else{
+                this.pokemon = pokemons[pokemons.length - 1];
+                pokemonId.value = pokemons.length - 1;
+            }
+            if(this.pokemon.type_secondary){
+                this.cores = {"background-image": "linear-gradient(" + getTypeColor(this.pokemon.type_primary) + ", " + getTypeColor(this.pokemon.type_secondary) +")"};
+            }else{
+                this.cores = {"background-image": "linear-gradient(" + getTypeColor(this.pokemon.type_primary) + ", " + getTypeColor(this.pokemon.type_primary) +")"};
+            }
+        }
+    }
+});
+
+function getTypeColor(type){
+    switch(type){
+        case "Normal":
+            return "#A8A878";
+        case "Fire":
+            return "#F08030";
+        case "Fighting":
+            return "#C03028";
+        case "Water":
+            return "#6890F0";
+        case "Flying":
+            return "#A890F0";
+        case "Grass":
+            return "#78C850";
+        case "Poison":
+            return "#A040A0";
+        case "Electric":
+            return "#F8D030";
+        case "Ground":
+            return "#E0C068";
+        case "Psychic":
+            return "#F85888";
+        case "Rock":
+            return "#B8A038";
+        case "Ice":
+            return "#98D8D8";
+        case "Bug":
+            return "#A8B820";
+        case "Dragon":
+            return "#7038F8";
+        case "Ghost":
+            return "#705898";
+        case "Dark":
+            return "#705848";
+        case "Steel":
+            return "#B8B8D0";
+        case "Fairy":
+            return "#EE99AC";
+        default:
+            return "#68A090";
+    }
+}
