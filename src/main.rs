a8425ded5b1a3b7324f1726be5c1b986da751869
Igor Feitosa Sampaio <igorfs10@gmail.com>
2020-10-03 22:47:40 -0300

    verifica se o arquivo já existe para não baixar
    exceto se tenha dado erro e salvo arquivo de 0 bytes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs::create_dir_all;
+use std::io::Read;
+use std::path::Path;
 
 use photon_rs::PhotonImage;
 use photon_rs::transform;
 use photon_rs::native::save_image;
+use rayon::prelude::*;
+use ureq::Agent;
 
 const PRIMEIRO_POKEMON: u16 = 1;
 const ULTIMO_POKEMON: u16 = 893;
@@ -22,26 +26,25 @@ fn main() {
 
     let caminho_imagens = format!("{}/{}", caminho_programa, PASTA_POKEMON);
 
-    let cliente = reqwest::blocking::Client::new();
+    let cliente = Agent::new().build();
 
     match create_dir_all(&caminho_imagens) {
         Ok(_) => {
-            pokemons.into_iter().for_each(|id| {
-                let pokemon_id = format!("{:03}", id);
-                let url_pokemon = format!("{}{}.png", URL, pokemon_id);
-
-                let resp = cliente.get(&url_pokemon).send().expect("Erro ao baixar o arquivo.");
-
-                let bytes = resp.bytes().unwrap().to_vec();
-                let mut imagem = PhotonImage::new_from_byteslice(bytes);
-
-                imagem = transform::resize(&imagem, COMPRIMENTO, ALTURA, transform::SamplingFilter::Lanczos3);
-
+            pokemons.into_par_iter().for_each(|id| {
                 let caminho_salvar = format!("{}/{}.png", caminho_imagens, id);
+                let full_path = Path::new(&caminho_salvar);
+                if full_path.exists() {
+                    if full_path.metadata().unwrap().len() > 0 {
+                        println!("{} já existe. Ignorando download.", id);
+                    }
+                    else {
+                        baixar_imagem(id, &caminho_salvar, &cliente);
+                    }
+                    
+                } else {
+                    baixar_imagem(id, &caminho_salvar, &cliente);
+                }
 
-                save_image(imagem, &caminho_salvar);
-
-                println!("{} baixado.", id);
             });
             println!("Fim.");
         }
@@ -49,4 +52,22 @@ fn main() {
             println!("Não foi possível criar a pasta.");
         }
     }
+}
+
+fn baixar_imagem (id: u16, caminho: &String, cliente: &Agent) {
+    let pokemon_id = format!("{:03}", id);
+    let url_pokemon = format!("{}{}.png", URL, pokemon_id);
+                    
+    let resp = cliente.get(&url_pokemon).call();
+
+    let mut reader = resp.into_reader();
+    let mut bytes = vec![];
+    let _ = reader.read_to_end(&mut bytes);
+    let mut imagem = PhotonImage::new_from_byteslice(bytes);
+
+    imagem = transform::resize(&imagem, COMPRIMENTO, ALTURA, transform::SamplingFilter::Lanczos3);
+
+    save_image(imagem, &caminho);
+
+    println!("{} baixado.", id);
 }
