14bd6c8475ddab1174e4e11b41d6291fcc20a46e
Igor Feitosa Sampaio <igorfs10@gmail.com>
2021-05-22 00:29:21 -0300

    Update generate_api.py

--- generate_api.py
+++ generate_api.py
@@ -25,9 +25,9 @@ for i in range (PRIMEIRO_POKEMON, ULTIMO_POKEMON + 1):
     data = dataset.iloc[i-1:i].to_json(orient = "records").replace("[", "").replace("]", "").replace("\/", "/").replace(".0", "")
     id = str(dataset.iloc[i-1:i,0].values[0])
     name = str(dataset.iloc[i-1:i,1].values[0])
-    with open("api/" + id +".json", "w") as file:
+    with open("api/" + id +"/index.json", "w") as file:
         file.write(data)
-    with open("api/" + name.lower() +".json", "w") as file:
+    with open("api/" + name.lower() +"/index.json", "w") as file:
         file.write(data)
     print(id + " generated.")
 
@@ -36,9 +36,6 @@ data = dataset[["id", "name", "color", "type_primary", "type_secondary", "image"
 data = '{"pokemons":[' + data + ']}'
 
 #salva a string no arquivo
-with open("api/pokemons.json", "w") as file:
-    file.write(data)
-
 with open("api/index.json", "w") as file:
     file.write(data)
 
